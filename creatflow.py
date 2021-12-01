1e2b3b37de2737169ecc164767c1678283056fed
jhzhou <jhzhou221700@163.com>
2021-12-01 22:40:26 +0800

    项目创建流程

---
+++ creatflow.py
@@ -0,0 +1,14 @@
+"""
+1.创建项目
+Django-admin startproject projectname
+2.创建app
+python manege.py startapp book(appname)
+3.到pycharm中设置项目的虚拟环境（初期创建项目的那个环境）
+4.setting中注册模型
+5.views视图 即为python函数 请求request与相应httpresponse
+6.路由定义 子应用urls文件的创建
+    from django.urls import path
+    以及项目路由的索引setting.py中 base_dir
+
+
+"""
