ed478a485e49a7b1e0669af73bc79ecbfcac0d3c
jhzhou <jhzhou221700@163.com>
2021-12-07 23:17:36 +0800

    查询功能 异常捕获 try except 以及 get filter exclude

--- book/views.py
+++ book/views.py
@@ -66,3 +66,56 @@ def index(request):
 # 方式2
 # Bookinfo.objects.filter(id=5).delete()
 # Create your views here.
+
+
+#########查询#######
+# get查询单一结果，如果不存在会抛出模型类.DoesNotExist异常。
+# book=Bookinfo.objects.get(id=1)
+# 出现异常的处理：若果不存在，，抛出异常
+# try:
+#     book=Bookinfo.objects.get(id=1)
+# except Bookinfo.DoesNotExist:
+#     print("查询结果不存在")
+# all查询多个结果。
+# books=Bookinfo.objects.all()
+# from book.models import Peopleinfo
+# Peopleinfo.objects.all()
+# count查询结果数量
+# Peopleinfo.objects.all().count()
+# 计算查询得到的总数据
+# Peopleinfo.objects.count()
+
+
+###过滤查询#####
+#
+# 实现SQL中的where功能，包括
+#
+# filter过滤出多个结果
+# exclude排除掉符合条件剩下的结果
+# get过滤单一结果
+
+#模型类名.objects.filter(属性名__运算符=值) 获取多个结果
+#模型类名.objects.exclude(属性名__运算符=值) 排除 not
+#模型类名.objects.get(属性名__运算符=值) 1个结果或异常
+
+#测试
+# 查询编号为1的图书
+# Bookinfo.objects.get(id=1) 简写
+# Bookinfo.objects.get(id__exact=1) 完整表示
+# Bookinfo.objects.get(pk=1) primary key 主键值等于1
+# Bookinfo.objects.filter(id=1) 返回的是包含一个元素的列表
+# 查询书名包含'湖'的图书
+Bookinfo.objects.get(name__contains="湖")
+# 查询书名以'部'结尾的图书
+Bookinfo.objects.get(name__endswith="部")
+# 查询书名为空的图书
+try:
+    Bookinfo.objects.get(name__exact="")
+except Bookinfo.DoesNotExist:
+    print("不存在")
+# 查询编号为1或3或5的图书
+Bookinfo.objects.filter(id__in=[1,3,5])
+# 查询编号大于3的图书
+# Bookinfo.objects.filter(id__gt=3) 大于gt 小于lt 大于等于
+# 查询1980年发表的图书
+# 查询1990年1月1日后发表的图书
